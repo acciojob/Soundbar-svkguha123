ac8c034dbab1c98b274b7f1b472a34394b4c6547
Souvik Guha <svkguha123@gmail.com>
2023-04-28 12:49:03 +0530

    up

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+const sounds = [];
+
+for (let i = 1; i <= 6; i++) {
+  sounds.push(new Promise((resolve, reject) => {
+    const sound = new Audio(`sounds/audio${i}.mp3`);
+    sound.addEventListener('canplay', () => {
+      resolve(sound);
+    });
+    sound.addEventListener('error', (error) => {
+      reject(error);
+    });
+  }));
+}
+
+Promise.all(sounds)
+  .then((loadedSounds) => {
+    const buttons = document.querySelectorAll('.btn');
+    const stopButton = document.querySelector('.stop');
+
+    function playSound(index) {
+      stopSounds();
+      loadedSounds[index].play();
+    }
+
+    function stopSounds() {
+      loadedSounds.forEach((sound) => {
+        sound.pause();
+        sound.currentTime = 0;
+      });
+    }
+
+    buttons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        playSound(index);
+      });
+    });
+
+    stopButton.addEventListener('click', () => {
+      stopSounds();
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to load sounds', error);
+  });
